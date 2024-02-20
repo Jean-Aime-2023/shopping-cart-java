cc42d0ad5c9d51966bf64eb3fdf8a4efea48c7f8
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-02-20 15:07:33 +0200

    Buy Now Function from the Shopping cart in JSP and Servlet-2

--- src/main/java/cn/tut/servlet/OrderNowServlet.java
+++ src/main/java/cn/tut/servlet/OrderNowServlet.java
@@ -7,6 +7,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import cn.tut.connection.DBConn;
+import cn.tut.dao.OrderDao;
+import cn.tut.model.Cart;
+import cn.tut.model.Order;
+import cn.tut.model.User;
 
 @WebServlet(name = "OrderNowServlet", value = "/order-now")
 public class OrderNowServlet extends HttpServlet {
@@ -15,8 +25,52 @@ public class OrderNowServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		try(PrintWriter out = response.getWriter()){
-			out.print("this is a buy now servlet");
-		}
+			
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");	
+		
+		Date date = new Date();
+			
+		 User auth = (User)	request.getSession().getAttribute("auth");
+		 if(auth != null) {
+			 String productId = request.getParameter("id");
+			 int productQuantity = Integer.parseInt(request.getParameter("quantity"));
+			 if(productQuantity <= 0) {
+				 productQuantity = 1;
+			 }
+			 
+			 Order orderModel = new Order();
+			 orderModel.setId(Integer.parseInt(productId));
+			 orderModel.setId(auth.getId());
+			 orderModel.setQuantity(productQuantity);
+			 orderModel.setDate(formatter.format(date));
+			 
+			 OrderDao orderDao = new OrderDao(DBConn.getConnection());
+			 boolean result = orderDao.insertOrder(orderModel);
+			 
+			 if(result) {
+				 
+				 ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+					if(cart_list != null) {
+						for(Cart c:cart_list) {
+							if(c.getId()==Integer.parseInt(productId)) {
+								cart_list.remove(cart_list.indexOf(c));
+								break;
+							}
+						}
+					}
+				 response.sendRedirect("orders.jsp");
+			 }else {
+				 out.print("order failed");
+			 }
+			 
+			 
+		 }else {
+			 response.sendRedirect("login.jsp");
+		 }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
